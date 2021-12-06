af0e1f92d30117cca2e767d3ee88eed5505ce51a
Tom Feron <tho.feron@gmail.com>
2021-12-06 17:14:50 +0100

    Extra info when denying access

--- src/Control/SafeAccess.hs
+++ src/Control/SafeAccess.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TupleSections #-}
 
 module Control.SafeAccess
   ( Capability(..)
@@ -24,108 +25,116 @@ import Control.Monad.Reader
 import Control.Monad.State
 import Control.Monad.Trans.Cont (ContT(..))
 import Control.Monad.Trans.Identity
-import Control.Monad.Trans.List
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.RWS (RWST(..))
 import Control.Monad.Writer
+import Data.Bifunctor
 
 -- | Allow things to be accessed. See 'ensureAccess'.
 --
 -- @d@ is the type describing an access.
-newtype Capability m d = MkCapability { runCapability :: d -> m AccessDecision }
+newtype Capability m d b
+  = MkCapability { runCapability :: d -> m (AccessDecision b) }
 
-instance Applicative m => Semigroup (Capability m d) where
+instance Applicative m => Semigroup (Capability m d b) where
   (<>) cap1 cap2 = MkCapability $ \d ->
     (<>) <$> runCapability cap1 d <*> runCapability cap2 d
 
-instance Applicative m => Monoid (Capability m d) where
+instance Applicative m => Monoid (Capability m d b) where
   mempty = MkCapability $ \_ -> pure mempty
   mappend = (<>)
 
-type Capabilities m d = [Capability m d]
+type Capabilities m d b = [Capability m d b]
 
 -- | Create a capability which only allows a given access
-singleCapability :: (Applicative f, Eq d) => d -> Capability f d
+singleCapability :: (Applicative f, Eq d) => d -> Capability f d b
 singleCapability = someCapabilities . pure
 
 -- | Create a capability which only allows given accesses
-someCapabilities :: (Applicative f, Eq d) => [d] -> Capability f d
+someCapabilities :: (Applicative f, Eq d) => [d] -> Capability f d b
 someCapabilities ds = MkCapability $ \d ->
-  pure $ if d `elem` ds then AccessGranted else AccessDeniedSoft
+  pure $ if d `elem` ds then AccessGranted else mempty
 
 -- | A special capability which allows every access. Be careful with this!
-passthroughCapability :: Applicative f => Capability f d
+passthroughCapability :: Applicative f => Capability f d b
 passthroughCapability = MkCapability $ \_ -> pure AccessGranted
 
 -- | Control the decision process.
 --
 -- The constructors are ordered by prevalence. For instance, if two capabilities
--- respectively return 'AccessGranted' and 'AccessDenied',
--- the final decision will be 'AccessDenied'.
-data AccessDecision
-  = AccessDeniedSoft  -- ^ No but another 'Capability' can still decide to grant
-  | AccessGranted     -- ^ Final yes (see explanation)
-  | AccessDenied      -- ^ Final no
+-- respectively return 'AccessGranted' and 'AccessDenied', the final decision
+-- will be 'AccessDenied'.
+--
+-- When denying access, additional information can be passed along.
+data AccessDecision b
+  = AccessDeniedSoft [b] -- ^ No but another 'Capability' can still decide to grant
+  | AccessGranted        -- ^ Final yes (see explanation)
+  | AccessDenied     [b] -- ^ Final no
   deriving (Show, Eq)
 
-instance Semigroup AccessDecision where
+instance Semigroup (AccessDecision b) where
   a <> b = case (a, b) of
-    (AccessDeniedSoft, _)          -> b
-    (_, AccessDeniedSoft)          -> a
-    (AccessDenied, _)              -> AccessDenied
-    (_, AccessDenied)              -> AccessDenied
-    (AccessGranted, AccessGranted) -> AccessGranted
+    (AccessGranted, AccessDenied _) -> b
+    (AccessGranted, _) -> AccessGranted
 
-instance Monoid AccessDecision where
-  mempty = AccessDeniedSoft
+    (AccessDenied xs, AccessDenied ys) -> AccessDenied (xs ++ ys)
+    (AccessDenied _, _) -> a
+
+    (AccessDeniedSoft _, AccessGranted) -> AccessGranted
+    (AccessDeniedSoft _, AccessDenied _) -> b
+    (AccessDeniedSoft xs, AccessDeniedSoft ys) -> AccessDeniedSoft (xs ++ ys)
+
+instance Monoid (AccessDecision b) where
+  mempty = AccessDeniedSoft []
   mappend = (<>)
 
 -- | A simple monad transformer to ensure that data are accessed legitimately.
 --
 -- The return value is either the description of an access having been denied
 -- (left) or the result of the normal computation (right).
-newtype SafeAccessT d m a
-  = SafeAccessT { runSafeAccessT :: Capabilities m d -> m (Either d a) }
+newtype SafeAccessT d b m a
+  = SafeAccessT { runSafeAccessT :: Capabilities m d b -> m (Either (d, [b]) a) }
 
-instance Functor f => Functor (SafeAccessT d f) where
+instance Functor f => Functor (SafeAccessT d b f) where
   fmap f sa = SafeAccessT $ \caps -> fmap (fmap f) $ runSafeAccessT sa caps
 
-instance Applicative f => Applicative (SafeAccessT d f) where
+instance Applicative f => Applicative (SafeAccessT d b f) where
   pure = SafeAccessT . const . pure . Right
   safef <*> safea = SafeAccessT $ \caps ->
     let fef = runSafeAccessT safef caps
         fea = runSafeAccessT safea caps
         ff  = flip fmap fef $ \ef -> case ef of
-                Left d  -> const $ Left d
-                Right f -> fmap f
+                Left err -> const $ Left err
+                Right f  -> fmap f
     in ff <*> fea
 
-instance Monad m => Monad (SafeAccessT d m) where
+instance Monad m => Monad (SafeAccessT d b m) where
   return = SafeAccessT . const . return . Right
   ma >>= f = SafeAccessT $ \caps -> do
     ex <- runSafeAccessT ma caps
     case ex of
-      Left d  -> return $ Left d
-      Right x -> runSafeAccessT (f x) caps
+      Left err -> return $ Left err
+      Right x  -> runSafeAccessT (f x) caps
 
-instance MonadTrans (SafeAccessT d) where
+instance MonadTrans (SafeAccessT d b) where
   lift = SafeAccessT . const . (Right `liftM`)
 
-instance MonadIO m => MonadIO (SafeAccessT d m) where
+instance MonadIO m => MonadIO (SafeAccessT d b m) where
   liftIO = SafeAccessT . const . (Right `liftM`) . liftIO
 
 -- | Check that the access is legal or make the monad \"fail\".
-ensureAccess :: MonadSafeAccess d m s => d -> m ()
+ensureAccess :: MonadSafeAccess d b m s => d -> m ()
 ensureAccess descr = do
   caps      <- getCapabilities
   decisions <- liftSub $ mapM (\cap -> runCapability cap descr) caps
   case mconcat decisions of
-    AccessGranted -> return ()
-    _             -> denyAccess descr
+    AccessGranted       -> return ()
+    AccessDenied xs     -> denyAccess descr xs
+    AccessDeniedSoft xs -> denyAccess descr xs
 
 -- | Allow certain accesses regardless of the capabilities. (unsecure!)
-unsecureAllow :: (Monad m, Eq d) => [d] -> SafeAccessT d m a
-              -> SafeAccessT d m a
+unsecureAllow :: (Monad m, Eq d) => [d] -> SafeAccessT d b m a
+              -> SafeAccessT d b m a
 unsecureAllow descrs action = do
   caps <- getCapabilities
   let cap = MkCapability $ \descr ->
@@ -134,52 +143,56 @@ unsecureAllow descrs action = do
           else mconcat <$> sequenceA (map (flip runCapability descr) caps)
   SafeAccessT $ \_ -> runSafeAccessT action [cap]
 
-getCapabilities' :: Monad m => SafeAccessT d m (Capabilities m d)
+getCapabilities' :: Monad m => SafeAccessT d b m (Capabilities m d b)
 getCapabilities' = SafeAccessT $ return . Right
 
-denyAccess' :: Monad m => d -> SafeAccessT d m ()
-denyAccess' = SafeAccessT . const . return . Left
+denyAccess' :: Monad m => d -> [b] -> SafeAccessT d b m ()
+denyAccess' = curry $ SafeAccessT . const . return . Left
 
-catchAccessError' :: Monad m => SafeAccessT d m a -> (d -> SafeAccessT d m a)
-                  -> SafeAccessT d m a
+catchAccessError'
+  :: Monad m
+  => SafeAccessT d b m a
+  -> (d -> [b] -> SafeAccessT d b m a)
+  -> SafeAccessT d b m a
 catchAccessError' action handler = SafeAccessT $ \caps -> do
   eRes <- runSafeAccessT action caps
   case eRes of
-    Left  descr -> runSafeAccessT (handler descr) caps
-    Right _     -> return eRes
+    Left  (descr, xs) -> runSafeAccessT (handler descr xs) caps
+    Right _ -> return eRes
 
-class (Monad m, Monad s) => MonadSafeAccess d m s | m -> s, m -> d where
-  getCapabilities  :: m (Capabilities s d)
+class (Monad m, Monad s) => MonadSafeAccess d b m s | m -> s, m -> d, m -> b where
+  getCapabilities  :: m (Capabilities s d b)
   liftSub          :: s a -> m a
-  denyAccess       :: d -> m ()
+  denyAccess       :: d -> [b] -> m ()
 
   -- | Catch an access error, i.e. an access descriptor which resulted into an
   -- access denied given the capabilities.
-  catchAccessError :: m a -> (d -> m a) -> m a
+  catchAccessError :: m a -> (d -> [b] -> m a) -> m a
 
-instance Monad m => MonadSafeAccess d (SafeAccessT d m) m where
+instance Monad m => MonadSafeAccess d b (SafeAccessT d b m) m where
   getCapabilities  = getCapabilities'
   liftSub          = lift
   denyAccess       = denyAccess'
   catchAccessError = catchAccessError'
 
 -- | Lift an action from 'ErrorT' to 'SafeAccessT'.
-liftExceptT :: ExceptT d m a -> SafeAccessT d m a
-liftExceptT = SafeAccessT . const . runExceptT
+liftExceptT :: Functor m => ExceptT d m a -> SafeAccessT d b m a
+liftExceptT action = SafeAccessT $ \_ ->
+  fmap (first (,[])) . runExceptT $ action
 
-instance MonadError e m => MonadError e (SafeAccessT d m) where
+instance MonadError e m => MonadError e (SafeAccessT d b m) where
   throwError     = lift . throwError
   catchError a h = SafeAccessT $ \caps ->
     catchError (runSafeAccessT a caps) (\e -> runSafeAccessT (h e) caps)
 
-instance MonadReader r m => MonadReader r (SafeAccessT d m) where
+instance MonadReader r m => MonadReader r (SafeAccessT d b m) where
   ask       = lift ask
   local f a = SafeAccessT $ local f . runSafeAccessT a
 
-instance MonadState s m => MonadState s (SafeAccessT d m) where
+instance MonadState s m => MonadState s (SafeAccessT d b m) where
   state = lift . state
 
-instance MonadWriter w m => MonadWriter w (SafeAccessT d m) where
+instance MonadWriter w m => MonadWriter w (SafeAccessT d b m) where
   writer = lift . writer
 
   listen sma = SafeAccessT $ \caps -> do
@@ -197,74 +210,75 @@ instance MonadWriter w m => MonadWriter w (SafeAccessT d m) where
     pass mpe
 
 liftCapability :: (Monad m, MonadTrans t)
-               => Capability m d -> Capability (t m) d
+               => Capability m d b -> Capability (t m) d b
 liftCapability cap = MkCapability $ \descr -> lift $ runCapability cap descr
 
-instance MonadSafeAccess d m s => MonadSafeAccess d (ContT e m) s where
+instance MonadSafeAccess d b m s => MonadSafeAccess d b (ContT e m) s where
   getCapabilities = lift getCapabilities
   liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
+  denyAccess d xs = lift $ denyAccess d xs
   catchAccessError action handler = ContT $ \cont ->
     catchAccessError (runContT action cont)
-                     (\descr -> runContT (handler descr) cont)
+                     (\descr xs -> runContT (handler descr xs) cont)
 
-instance MonadSafeAccess d m s => MonadSafeAccess d (ExceptT e m) s where
+instance MonadSafeAccess d b m s => MonadSafeAccess d b (ExceptT e m) s where
   getCapabilities = lift getCapabilities
   liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
+  denyAccess d xs = lift $ denyAccess d xs
   catchAccessError action handler = ExceptT $
-    catchAccessError (runExceptT action) (runExceptT . handler)
+    catchAccessError
+      (runExceptT action)
+      (\descr xs -> runExceptT (handler descr xs))
 
-instance MonadSafeAccess d m s => MonadSafeAccess d (StateT s' m) s where
+instance MonadSafeAccess d b m s => MonadSafeAccess d b (StateT s' m) s where
   getCapabilities = lift getCapabilities
   liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
+  denyAccess d xs = lift $ denyAccess d xs
   catchAccessError action handler = StateT $ \s ->
     catchAccessError (runStateT action s)
-                     (\descr -> runStateT (handler descr) s)
+                     (\descr xs -> runStateT (handler descr xs) s)
 
-instance MonadSafeAccess d m s => MonadSafeAccess d (IdentityT m) s where
+instance MonadSafeAccess d b m s => MonadSafeAccess d b (IdentityT m) s where
   getCapabilities = lift getCapabilities
   liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
+  denyAccess d xs = lift $ denyAccess d xs
   catchAccessError action handler = IdentityT $
-    catchAccessError (runIdentityT action) (runIdentityT . handler)
+    catchAccessError
+      (runIdentityT action)
+      (\descr xs -> runIdentityT (handler descr xs))
 
-instance MonadSafeAccess d m s => MonadSafeAccess d (ListT m) s where
+instance MonadSafeAccess d b m s => MonadSafeAccess d b (MaybeT m) s where
   getCapabilities = lift getCapabilities
   liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
-  catchAccessError action handler = ListT $
-    catchAccessError (runListT action) (runListT . handler)
-
-instance MonadSafeAccess d m s => MonadSafeAccess d (MaybeT m) s where
-  getCapabilities = lift getCapabilities
-  liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
+  denyAccess d xs = lift $ denyAccess d xs
   catchAccessError action handler = MaybeT $
-    catchAccessError (runMaybeT action) (runMaybeT . handler)
+    catchAccessError
+      (runMaybeT action)
+      (\descr xs -> runMaybeT (handler descr xs))
 
-instance MonadSafeAccess d m s => MonadSafeAccess d (ReaderT r m) s where
+instance MonadSafeAccess d b m s => MonadSafeAccess d b (ReaderT r m) s where
   getCapabilities = lift getCapabilities
   liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
+  denyAccess d xs = lift $ denyAccess d xs
   catchAccessError action handler = ReaderT $ \r ->
     catchAccessError (runReaderT action r)
-                     (\descr -> runReaderT (handler descr) r)
+                     (\descr xs -> runReaderT (handler descr xs) r)
 
-instance (MonadSafeAccess d m s, Monoid w)
-    => MonadSafeAccess d (RWST r w st m) s where
+instance (MonadSafeAccess d b m s, Monoid w)
+    => MonadSafeAccess d b (RWST r w st m) s where
   getCapabilities = lift getCapabilities
   liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
+  denyAccess d xs = lift $ denyAccess d xs
   catchAccessError action handler = RWST $ \r s ->
     catchAccessError (runRWST action r s)
-                     (\descr -> runRWST (handler descr) r s)
+                     (\descr xs -> runRWST (handler descr xs) r s)
 
-instance (MonadSafeAccess d m s, Monoid w)
-    => MonadSafeAccess d (WriterT w m) s where
+instance (MonadSafeAccess d b m s, Monoid w)
+    => MonadSafeAccess d b (WriterT w m) s where
   getCapabilities = lift getCapabilities
   liftSub         = lift . liftSub
-  denyAccess      = lift . denyAccess
+  denyAccess d xs = lift $ denyAccess d xs
   catchAccessError action handler = WriterT $
-    catchAccessError (runWriterT action) (runWriterT . handler)
+    catchAccessError
+      (runWriterT action)
+      (\descr xs -> runWriterT (handler descr xs))
